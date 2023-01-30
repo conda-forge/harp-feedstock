2d5915479c1f79a1632d77cb84e1dd8f888f93e2
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2023-01-30 17:54:36 +0100

    Use cmake --install

--- recipe/install.bat
+++ recipe/install.bat
@@ -1,11 +1,11 @@
 if [%PKG_NAME%] == [harp] (
-  cmake --build . --component core --target install --config Release
+  cmake --install . --component core --config Release
   if errorlevel 1 exit 1
-  cmake --build . --component python --target install --config Release
+  cmake --install . --component python --config Release
   if errorlevel 1 exit 1
 )
 
 if [%PKG_NAME%] == [r-harp] (
-  cmake --build . --component r --target install --config Release
+  cmake --install . --component r --config Release
   if errorlevel 1 exit 1
 )
