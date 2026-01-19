ad25a9e821fa3b16c49361731cb47ded3e48fc20
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2026-01-19 17:49:58 +0100

    Repeat cmake arguments for R build

--- recipe/install_r.bat
+++ recipe/install_r.bat
@@ -1,4 +1,11 @@
-cmake -DHARP_BUILD_R=True
+cmake ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DHARP_ENABLE_CONDA_INSTALL=True ^
+  -DHARP_BUILD_R=True ^
+  -DCODA_INCLUDE_DIR="%LIBRARY_INC%" ^
+  -DHDF4_INCLUDE_DIR="%LIBRARY_INC%" ^
+  -DHDF5_INCLUDE_DIR="%LIBRARY_INC%" ^
+  -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX%
 if errorlevel 1 exit 1
 
 cmake --install . --component r --config Release
--- recipe/install_r.sh
+++ recipe/install_r.sh
@@ -5,6 +5,13 @@ set -x
 # Reuse build directory of regular HARP build
 cd build
 
-cmake $CMAKE_ARGS -DHARP_BUILD_R=True ..
+cmake $CMAKE_ARGS \
+  -DCMAKE_BUILD_TYPE="Release" \
+  -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+  -DCMAKE_PREFIX_PATH="$PREFIX" \
+  -DHARP_BUILD_R=True \
+  -DCODA_INCLUDE_DIR="$PREFIX/include" \
+  -DCODA_LIBRARY_DIR="$PREFIX/lib" \
+  ..
 make -j$CPU_COUNT
 cmake --install . --component r
